d7f93ece64c9a14c88e48584fc44bf14102877b6
Shanessa Kostaman <Shanessa.m7493@gmail.com>
2022-09-22 11:22:02 +0700

    creating the connection to the database for adding passanger

--- controllers/ProfileController.php
+++ controllers/ProfileController.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace app\controllers;
+use Yii;
+use app\models\person;
 
 class ProfileController extends \yii\web\Controller
 {
@@ -8,5 +10,23 @@ class ProfileController extends \yii\web\Controller
     {
         return $this->render('index');
     }
-
+    public function actionSave(){
+        $model = new person();
+        if(Yii::$app->request->isAjax){
+            $data = Yii::$app->request->post();
+            $model->first_name = $data['last_name'];
+            $model->last_name = $data['last_name'];
+            $model->date_of_birth = $data['date_of_birth'];
+            $model->gender = $data['gender'];
+            $model->nationality = $data['nationality'];
+            $model->personal_doc_type = $data['personal_doc_type'];
+            $model->personal_doc_num = $data['personal_doc_num'];
+                $model->save();
+                return "OK";    
+            
+        }
+        else {
+            return 'error';
+        }
+    }
 }
--- views/profile/index.php
+++ views/profile/index.php
@@ -16,27 +16,65 @@ $this->registerCssFile("@web/css/profile.css")
   </head> -->
 
 <body>
-    <div class="Background" >
-        <div class="LoginForm">
-            <form method="post" ,action="Login.html">
-                <div class="row">
-                    <h2 class="LoginTitle" style="text-align: left">
-                        My Profile
-                    </h2>
+    <div class="Background">
+        <div class='ProfileForms'>
+            <div class="LoginForm1">
+                <form method="post" ,action="Login.html">
+                    <div class="row">
+                        <h2 class="LoginTitle" style="text-align: left">
+                            My Profile
+                        </h2>
 
-                    <div class="col">
-                        <div class="hide-md-lg">
-                            <!-- <p>Sign Up manually:</p> -->
+                        <div class="col">
+                            <div class="hide-md-lg">
+                                <!-- <p>Sign Up manually:</p> -->
+                            </div>
+                            <h3>Email :</h3>
+                            <input type="text" name="email" id="email" placeholder="Email" required />
+                            <h3>Confirm Password :</h3>
+                            <input type="password" name="password" id="password" placeholder="Password" required />
+                            <br></br>
+                            <input type="button" value="Edit" id="login" />
                         </div>
-                        <h3>Email :</h3>
-                        <input type="text" name="email" id="email" placeholder="Email" required />
-                        <h3>Confirm Password :</h3>
-                        <input type="password" name="password" id="password" placeholder="Password" required />
-                        <br></br>
-                        <input type="button" value="Edit" id="login" />
                     </div>
+                </form>
+            </div>
+            <div class="LoginForm1">
+                <h2 class="LoginTitle" style="text-align: left">
+                    Passenger List
+                </h2>
+                <div class="tableStructure">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td colspan="2">Larry the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
-            </form>
+            </div>
         </div>
         <div class="LoginForm">
             <form method="post" ,action="Login.html">
@@ -48,55 +86,57 @@ $this->registerCssFile("@web/css/profile.css")
                     </div>
                 </div>
 
-                    <div class="col">
-                        <div class="hide-md-lg">
-                            <!-- <p>Sign Up manually:</p> -->
-                        </div>
-                        <h3>First Name :</h3>
-                        <input type="text" name="firstName" id="firstName" placeholder="First Name" required />
-                        <h3>Last Name :</h3>
-                        <input type="text" name="lastName" id="lastName" placeholder="Last Name" required />
-                        <h3>Date of Birth :</h3>
-                        <input type="datetime" name="dateOfBirth" id="dateOfBirth" placeholder="Date of Birth" required />
-                        <h3>Gender :</h3>
-                        <input type="text" name="gender" id="gender" placeholder="Gender" required />
-                        <h3>Nationality :</h3>
-                        <input type="text" name="nationality" id="nationality" placeholder="Nationality" required />
-                        <h3>Document Type :</h3>
-                        <input type="text" name="documentType" id="documentType" placeholder="Document Type" required />
-                        <h3>Document Number :</h3>
-                        <input type="text" name="documentNumber" id="documentNumber" placeholder="Document Number" required />
-                        <br></br>
-                        <input type="button" value="Add New Passanger" id="login" />
+                <div class="col">
+                    <div class="hide-md-lg">
+                        <!-- <p>Sign Up manually:</p> -->
                     </div>
+                    <h3>First Name :</h3>
+                    <input type="text" name="firstName" id="firstName" placeholder="First Name" required />
+                    <h3>Last Name :</h3>
+                    <input type="text" name="lastName" id="lastName" placeholder="Last Name" required />
+                    <h3>Date of Birth :</h3>
+                    <input type="datetime" name="dateOfBirth" id="dateOfBirth" placeholder="Date of Birth" required />
+                    <h3>Gender :</h3>
+                    <input type="text" name="gender" id="gender" placeholder="Gender" required />
+                    <h3>Nationality :</h3>
+                    <input type="text" name="nationality" id="nationality" placeholder="Nationality" required />
+                    <h3>Document Type :</h3>
+                    <input type="text" name="documentType" id="documentType" placeholder="Document Type" required />
+                    <h3>Document Number :</h3>
+                    <input type="text" name="documentNumber" id="documentNumber" placeholder="Document Number" required />
+                    <br></br>
+                    <input type="button" value="Add New Passanger" id="login" />
                 </div>
-            </form>
         </div>
+        </form>
+    </div>
+    </div>
     </div>
 
 
-    </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script>
-        $(document).ready(function() {
-            $("#login").on("click", function() {
-                var email = $("#email").val();
-                var password = $("#password").val();
-                var RepeatPassword = $("#repeatPassword").val();
-                $.ajax({
-                    url: '<?php echo Yii::$app->request->baseUrl . '/signup/save' ?>',
-                    type: "POST",
-                    data: {
-                        email: $("#email").val(),
-                        password: $("#password").val()
-                    },
-                    success: function(response) {
-                        console.log(response);
-                    },
-                });
-            });
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $("#login").on("click", function() {
+        $.ajax({
+          url: '<?php echo Yii::$app->request->baseUrl . '/signup/save' ?>',
+          type: "POST",
+          data: {
+            first_name : $("#firstName").val(),
+            last_name : $("#lastName").val(),
+            date_of_birth : $("#dateOfBirth").val(),
+            gender : $("#gender").val(),
+            nationality : $("#nationality").val(),
+            personal_doc_type : $("#docType").val(),
+            personal_doc_num : $("#docNumb").val()
+          },
+          success: function(response) {
+            console.log(response);
+          },
         });
-    </script>
+      });
+    });
+  </script>
 </body>
 
 </html>
